8787942be0bd31e273e908d6d504518424ee89a9
Sankar சங்கர் <psankar@vetchium.com>
2025-12-14 16:34:44 +0530

    validations

--- specs/typespec/hub/hubusers.go
+++ specs/typespec/hub/hubusers.go
@@ -1,11 +1,25 @@
 package hub
 
-type EmailAddress string
-type Password string
+import (
+	"vetchium-api-server.typespec/common"
+)
 
 type HubLoginRequest struct {
-	EmailAddress EmailAddress `json:"email_address"`
-	Password     Password     `json:"password"`
+	EmailAddress common.EmailAddress `json:"email_address"`
+	Password     common.Password     `json:"password"`
+}
+
+func (r HubLoginRequest) Validate() []common.ValidationError {
+	var errs []common.ValidationError
+
+	if err := r.EmailAddress.Validate(); err != nil {
+		errs = append(errs, common.NewValidationError("email_address", err))
+	}
+	if err := r.Password.Validate(); err != nil {
+		errs = append(errs, common.NewValidationError("password", err))
+	}
+
+	return errs
 }
 
 type HubLoginResponse struct {
--- specs/typespec/hub/hubusers.ts
+++ specs/typespec/hub/hubusers.ts
@@ -1,9 +1,33 @@
-export type EmailAddress = string;
-export type Password = string;
+import {
+	type EmailAddress,
+	type Password,
+	type ValidationError,
+	newValidationError,
+	validateEmailAddress,
+	validatePassword,
+} from "../common/common";
+
+export type { EmailAddress, Password, ValidationError };
 
 export interface HubLoginRequest {
 	email_address: EmailAddress;
 	password: Password;
+}
+
+export function validateHubLoginRequest(request: HubLoginRequest): ValidationError[] {
+	const errs: ValidationError[] = [];
+
+	const emailErr = validateEmailAddress(request.email_address);
+	if (emailErr) {
+		errs.push(newValidationError("email_address", emailErr));
+	}
+
+	const passwordErr = validatePassword(request.password);
+	if (passwordErr) {
+		errs.push(newValidationError("password", passwordErr));
+	}
+
+	return errs;
 }
 
 export interface HubLoginResponse {
