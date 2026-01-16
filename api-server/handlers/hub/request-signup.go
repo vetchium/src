9213ee69768547e2e9d134d818c7937b09ca6507
Sankar சங்கர் <sankar.curiosity@gmail.com>
2026-01-16 17:35:04 +0530

    Fix error code

--- api-server/handlers/hub/request-signup.go
+++ api-server/handlers/hub/request-signup.go
@@ -56,7 +56,7 @@ func RequestSignup(s *server.Server) http.HandlerFunc {
 		if err != nil {
 			if errors.Is(err, pgx.ErrNoRows) {
 				log.Debug("domain not approved", "domain", domain)
-				w.WriteHeader(http.StatusBadRequest)
+				w.WriteHeader(http.StatusForbidden)
 				return
 			}
 			log.Error("failed to query domain", "error", err)
