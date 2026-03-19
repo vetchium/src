5c8f5b90ef96b51e0c701139b06742d1798732da
Sankar சங்கர் <sankar.curiosity@gmail.com>
2026-03-19 21:20:00 +0530

    public route

--- api-server/internal/routes/global-routes.go
+++ api-server/internal/routes/global-routes.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"vetchium-api-server.gomodule/handlers/global"
+	publichandlers "vetchium-api-server.gomodule/handlers/public"
 	"vetchium-api-server.gomodule/internal/server"
 )
 
@@ -12,4 +13,5 @@ func RegisterGlobalRoutes(mux *http.ServeMux, s *server.Server) {
 	mux.HandleFunc("POST /global/get-regions", global.GetRegions(s))
 	mux.HandleFunc("POST /global/get-supported-languages", global.GetSupportedLanguages(s))
 	mux.HandleFunc("POST /global/check-domain", global.CheckDomain(s))
+	mux.HandleFunc("GET /public/tag-icon", publichandlers.GetTagIcon(s))
 }
